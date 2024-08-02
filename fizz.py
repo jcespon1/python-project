b9503c8b7cacdeaa34ffa18bed6ecc12323f9a82
jcespon1 <j.cespon1@gmail.com>
2024-08-02 20:48:54 +0100

    changed number

--- fizz.py
+++ fizz.py
@@ -2,7 +2,7 @@
 # print numbers from 1 to 100, but replace multiples of 3 with "Fizz" and 
 # multiples of 5 with "Buzz"
 
-for x in range(1,101):
+for x in range(1,105):
     if (x % 3 == 0) and (x % 5 == 0):
         print("FizzBuzz")
     elif (x % 3 == 0):
